21489fcc1c7f46d838a4df4cccd495e2937a30cc
OlenaSht <shteplekarelena@gmail.com>
2023-09-16 01:41:09 +0300

    test adding - 2

---
+++ about_commands.py
@@ -0,0 +1,12 @@
+# PS C:\Users\meinf\PycharmProjects\QA_7_Demo\QA7_Demo> git config --global user.email "shteplekarelena@gmail.com"
+# PS C:\Users\meinf\PycharmProjects\QA_7_Demo\QA7_Demo> git config --global user.name "OlenaSht"
+# PS C:\Users\meinf\PycharmProjects\QA_7_Demo\QA7_Demo> git commit -m "test functionality adding"
+# [main (root-commit) 43b8422] test functionality adding
+#  1 file changed, 2 insertions(+)
+#  create mode 100644 testfile.py
+# PS C:\Users\meinf\PycharmProjects\QA_7_Demo\QA7_Demo> git log
+# commit 43b84224605882f13f0189545fc637292c9b5ca8 (HEAD -> main)
+# Author: OlenaSht <shteplekarelena@gmail.com>
+# Date:   Sat Sep 16 01:24:12 2023 +0300
+#
+#     test functionality adding
